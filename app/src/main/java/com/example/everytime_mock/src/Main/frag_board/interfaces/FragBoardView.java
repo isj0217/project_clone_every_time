8bc8982945dac0ce195d4d692ed2b60008fa0bed
isj0217 <50730955+isj0217@users.noreply.github.com>
2020-07-08 15:55:59 +0900

    20200708 API 11 (즐겨찾기 추가/취소) 문제 해결중

---
+++ app/src/main/java/com/example/everytime_mock/src/Main/frag_board/interfaces/FragBoardView.java
@@ -0,0 +1,30 @@
+package com.example.everytime_mock.src.Main.frag_home.interfaces;
+
+import com.example.everytime_mock.src.Boards.models.BoardResponse;
+import com.example.everytime_mock.src.Main.frag_home.models.AdvertisementResponse;
+import com.example.everytime_mock.src.Main.frag_home.models.HotPostResponse;
+import com.example.everytime_mock.src.Main.frag_home.models.RealTimeHotPostResponse;
+import com.example.everytime_mock.src.Main.frag_home.models.RecentLectureReviewResponse;
+
+public interface FragHomeView {
+
+    void validateSuccess(String text);
+
+    void validateFailure(String message);
+
+    void advertisementSuccess(AdvertisementResponse advertisementResponse);
+
+    void realTimeHotPostSuccess(RealTimeHotPostResponse realTimeHotPostResponse);
+
+    void hotPostSuccess(HotPostResponse hotPostResponse);
+
+    void recentLectureReviewSuccess(RecentLectureReviewResponse recentLectureReviewResponse);
+
+    void getFreeBoardSuccess(BoardResponse boardResponse);
+
+    void getSecretBoardSuccess(BoardResponse boardResponse);
+
+    void getAlumniBoardSuccess(BoardResponse boardResponse);
+
+    void getFreshmenBoardSuccess(BoardResponse boardResponse);
+}
---
+++ app/src/main/java/com/example/everytime_mock/src/Main/frag_board/models/FavoriteBoardResult.java
@@ -0,0 +1,20 @@
+package com.example.everytime_mock.src.Main.frag_home.models;
+
+public class AdvertisementResult {
+
+    int adIdx;
+    String adThumbnaillURL;
+
+    public AdvertisementResult(int adIdx, String adThumbnaillURL) {
+        this.adIdx = adIdx;
+        this.adThumbnaillURL = adThumbnaillURL;
+    }
+
+    public int getAdIdx() {
+        return adIdx;
+    }
+
+    public String getAdThumbnaillURL() {
+        return adThumbnaillURL;
+    }
+}
