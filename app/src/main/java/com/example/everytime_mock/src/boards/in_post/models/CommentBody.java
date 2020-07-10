eaf994b4695368c01dfe059ac1452801ba7d6a7a
isj0217 <50730955+isj0217@users.noreply.github.com>
2020-07-10 13:30:19 +0900

    home화면 ->  댓글조회 / 게시판 화면 -> 댓글조회 구현완료. 댓글 추가 기능 미흡

---
+++ app/src/main/java/com/example/everytime_mock/src/boards/in_post/models/CommentBody.java
@@ -0,0 +1,37 @@
+package com.example.everytime_mock.src.signUp.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class SignUpBody {
+
+    public SignUpBody(String userID, String pw, String userNickname, String phoneNum, String univName, int univYear, String email) {
+        this.userID = userID;
+        this.pw = pw;
+        this.userNickname = userNickname;
+        this.phoneNum = phoneNum;
+        this.univName = univName;
+        this.univYear = univYear;
+        this.email = email;
+    }
+
+    @SerializedName("userID")
+    private String userID;
+
+    @SerializedName("pw")
+    private String pw;
+
+    @SerializedName("userNickname")
+    private String userNickname;
+
+    @SerializedName("phoneNum")
+    private String phoneNum;
+
+    @SerializedName("univName")
+    private String univName;
+
+    @SerializedName("univYear")
+    private int univYear;
+
+    @SerializedName("email")
+    private String email;
+}
